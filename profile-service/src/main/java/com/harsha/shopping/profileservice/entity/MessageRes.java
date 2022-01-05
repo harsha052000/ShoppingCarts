1ad38bd0e17c23f2f99ec6e5c2624302e3b648e6
harsha052000 <93130552+harsha052000@users.noreply.github.com>
2022-01-05 22:01:35 +0530

    profile service

---
+++ profile-service/src/main/java/com/harsha/shopping/profileservice/entity/MessageRes.java
@@ -0,0 +1,13 @@
+package com.harsha.shopping.profileservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class MessageRes {
+
+    private String message;
+}
